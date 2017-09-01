bb3b4a99aec806fd4111e280577eb22d245eddde
John-Michael L'Allier <jmlallier@me.com>
2017-08-31 23:00:57 -0400

    Add TasksController for index and show methods

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+
+	public function scopeIncomplete($query) {
+		return $query->where('completed', 0);
+	}
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,10 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-use App\Task;
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');
 
 Route::get( '/', function() {
 	$name = "JM";
 
 	return view( 'welcome', compact( 'name' ) );
-} );
-
-Route::get( '/tasks/{task?}', function( $id = NULL ) {
-	if ( !is_null( $id ) ) {
-		$task = Task::find($id);
-
-		return view( 'tasks.show', compact( 'task' ) );
-	} else {
-		$tasks = Task::all();
-
-		return view( 'tasks.index', compact( 'tasks' ) );
-	}
 } );
